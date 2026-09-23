ad3748c02c7c34d94b723cd434597430c4a6bfce
MaryN333 <sidyakina@gmail.com>
2026-09-23 12:38:28 +0200

    feat: add manual logging to BookService

--- src/main/java/cz/wz/marysidy/library/service/BookService.java
+++ src/main/java/cz/wz/marysidy/library/service/BookService.java
@@ -1,13 +1,22 @@
 package cz.wz.marysidy.library.service;
 
 import cz.wz.marysidy.library.entity.Book;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
+//@Slf4j
 @Service
 public class BookService {
 
+    private static final Logger log = LoggerFactory.getLogger(BookService.class);
+
     public Book getBookById(Long id) {
-        return new Book(1L, "Pushkin", "Title");
+        log.info("getBookById({}) method started ...", id);
+
+        Book book = new Book(1L, "Pushkin", "Title");
+
+        log.info("getBookById({}) method finished.", id);
+        return book;
     }
 }
